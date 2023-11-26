adaf838dd39057d1fbe7395ab2c7eea2acde0209
ValentinLH <98545666+ValentinLH@users.noreply.github.com>
2023-11-26 09:03:44 -0500

    feat: exec ok mais probleme dimension affichage plt.scatter et pcolormesh

--- Projet_de_Session_IFT712/Modele/Perceptron.py
+++ Projet_de_Session_IFT712/Modele/Perceptron.py
@@ -3,6 +3,7 @@ from .ClassifieurLineaire import StrategieClassification
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.calibration import LabelEncoder
+from sklearn.decomposition import PCA
 
 class Perceptron(StrategieClassification):
     def __init__(self, learning_rate=0.01, max_iterations=1000,penalty='l2'):
@@ -58,11 +59,48 @@ class Perceptron(StrategieClassification):
         return self.w_0, self.w
     
     def afficher(self, x_train, t_train, x_test, t_test):
-        
         le = LabelEncoder()
 
         # Encode training labels
         t_train_encoded = le.fit_transform(t_train)
+        t_test_encoded = le.transform(t_test)
+
+        h = 0.05  # Contrôle la résolution de la grille
+
+        # Utilisez PCA pour réduire les dimensions à 2 pour la représentation
+        pca = PCA(n_components=2)
+        x_train_2d = pca.fit_transform(x_train)
+
+        x_min, x_max = x_train_2d[:, 0].min() - .5, x_train_2d[:, 0].max() + .5
+        y_min, y_max = x_train_2d[:, 1].min() - .5, x_train_2d[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+        # Utilisez inverse_transform pour obtenir les coordonnées originales dans l'espace d'origine
+        X_predict = pca.inverse_transform(np.c_[xx.ravel(), yy.ravel()])
+
+        # Classifier chaque point de la grille
+        Z = self.perceptron_model.predict(X_predict)
+        Z = le.transform(Z)
+        Z = Z.reshape(xx.shape)
+
+        
+        plt.figure(figsize=(14, 8))
+        plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)  # Colorier les cases selon les prédictions
+
+        plt.scatter(x_train[:, 0], x_train[:, 1], c=t_train_encoded, edgecolors='k', cmap=plt.cm.Paired)  # Tracer les données
+
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xticks(())
+        plt.yticks(())
+
+        plt.title('Frontières de décision')
+        plt.show()
+        
+        return
+        
+        #visualizeClassificationAreas(self.perceptron_model,x_train, t_train_encoded,x_test, t_test_encoded)
+        
         plt.figure(0)
         plt.scatter(x_train[:, 0], x_train[:, 1], s=(t_train_encoded * 100 + 20).astype(int), c=t_train_encoded)
 
@@ -140,6 +178,91 @@ class Perceptron(StrategieClassification):
         plt.title('Testing data')
 
         plt.show()
+    
+def visualizeClassificationAreas(usedClassifier,XtrainSet, ytrainSet,XtestSet, ytestSet, plotDensity=0.01 ):
+    '''
+    https://aleksandarhaber.com/solve-classification-problems-in-python-scikit-learn-and-visualize-the-classification-results-machine-learning-tutorial/
+    This function visualizes the classification regions on the basis of the provided data
+
+    usedClassifier      - used classifier
+
+    XtrainSet,ytrainSet - train sets of features and target classes 
+                    - the region colors correspond to to the class numbers in ytrainSet
+
+    XtestSet,ytestSet   - test sets for visualizng the performance on test data that is not used for training
+                    - this is optional argument provided if the test data is available
+
+    plotDensity         - density of the area plot, smaller values are making denser and more precise plots
+
+    IMPORTANT COMMENT:  -If the number of classes is larger than the number of elements in the list 
+                    caller "colorList", you need to increase the number of colors in 
+                    "colorList"
+                    - The same comment applies to "markerClass" list defined below
+
+    '''
+    import numpy as np
+    import matplotlib.pyplot as plt    
+    # this function is used to create a "cmap" color object that is used to distinguish different classes
+    from matplotlib.colors import ListedColormap 
+    
+    # this list is used to distinguish different classification regions
+    # every color corresponds to a certain class number
+    colorList=["blue", "green", "orange", "magenta", "purple", "red"]
+    # this list of markers is used to distingush between different classe
+    # every symbol corresponds to a certain class number
+    markerClass=['x','o','v','#','*','>']
+    
+    # get the number of different classes 
+    classesNumbers=np.unique(ytrainSet)
+    numberOfDifferentClasses=classesNumbers.size
+                
+    # create a cmap object for plotting the decision areas
+    cmapObject=ListedColormap(colorList, N=numberOfDifferentClasses)
+                
+    # get the limit values for the total plot
+    x1featureMin=min(XtrainSet[:,0].min(),XtestSet[:,0].min())-0.5
+    x1featureMax=max(XtrainSet[:,0].max(),XtestSet[:,0].max())+0.5
+    x2featureMin=min(XtrainSet[:,1].min(),XtestSet[:,1].min())-0.5
+    x2featureMax=max(XtrainSet[:,1].max(),XtestSet[:,1].max())+0.5
+        
+    # create the meshgrid data for the classifier
+    x1meshGrid,x2meshGrid = np.meshgrid(np.arange(x1featureMin,x1featureMax,plotDensity),np.arange(x2featureMin,x2featureMax,plotDensity))
+        
+    # basically, we will determine the regions by creating artificial train data 
+    # and we will call the classifier to determine the classes
+    XregionClassifier = np.array([x1meshGrid.ravel(),x2meshGrid.ravel()]).T
+    # call the classifier predict to get the classes
+    predictedClassesRegion=usedClassifier.predict(XregionClassifier)
+    # the previous code lines return the vector and we need the matrix to be able to plot
+    predictedClassesRegion=predictedClassesRegion.reshape(x1meshGrid.shape)
+    # here we plot the decision areas
+    # there are two subplots - the left plot will plot the decision areas and training samples
+    # the right plot will plot the decision areas and test samples
+    fig, ax = plt.subplots(1,2,figsize=(15,8))
+    ax[0].contourf(x1meshGrid,x2meshGrid,predictedClassesRegion,alpha=0.3,cmap=cmapObject)
+    ax[1].contourf(x1meshGrid,x2meshGrid,predictedClassesRegion,alpha=0.3,cmap=cmapObject)
+    
+    # scatter plot of features belonging to the data set 
+    for index1 in np.arange(numberOfDifferentClasses):
+        ax[0].scatter(XtrainSet[ytrainSet==classesNumbers[index1],0],XtrainSet[ytrainSet==classesNumbers[index1],1], 
+                alpha=1, c=colorList[index1], marker=markerClass[index1], 
+                label="Class {}".format(classesNumbers[index1]), edgecolor='black', s=80)
+        
+        ax[1].scatter(XtestSet[ytestSet==classesNumbers[index1],0],XtestSet[ytestSet==classesNumbers[index1],1], 
+                alpha=1, c=colorList[index1], marker=markerClass[index1],
+                label="Class {}".format(classesNumbers[index1]), edgecolor='black', s=80)
+        
+    ax[0].set_xlabel("Feature X1",fontsize=14)
+    ax[0].set_ylabel("Feature X2",fontsize=14)
+    ax[0].text(0.05, 0.05, "Decision areas and training samples", transform=ax[0].transAxes, fontsize=14, verticalalignment='bottom')
+    ax[0].legend(fontsize=14)
+    
+    ax[1].set_xlabel("Feature X1",fontsize=14)
+    ax[1].set_ylabel("Feature X2",fontsize=14)
+    ax[1].text(0.05, 0.05, "Decision areas and test samples", transform=ax[1].transAxes, fontsize=14, verticalalignment='bottom')
+    #ax[1].legend()
+    plt.savefig('classification_results.png')
+
         
 """
     def afficher(self, x_train, t_train, x_test=None, t_test=None, feature_names=None, class_names=None):
