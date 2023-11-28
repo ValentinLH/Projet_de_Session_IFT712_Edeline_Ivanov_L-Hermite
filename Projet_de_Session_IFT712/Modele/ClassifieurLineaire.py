c0d7145401bd91e1d871485f05d0e13c08d82bfb
ValentinLH <98545666+ValentinLH@users.noreply.github.com>
2023-11-28 13:45:20 -0500

    affichage generale et reglage prédiction classifieur linéaire

--- Projet_de_Session_IFT712/Modele/ClassifieurLineaire.py
+++ Projet_de_Session_IFT712/Modele/ClassifieurLineaire.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.interpolate import LinearNDInterpolator
+from sklearn.calibration import LabelEncoder
 
 # Créez une classe abstraite pour la stratégie
 class StrategieClassification(ABC):   
@@ -39,6 +43,9 @@ class ClassifieurLineaire:
 
     def prediction(self, x):
         # Utilisez la stratégie pour la prédiction
+        if not isinstance(x, (list, np.ndarray)):
+            x = [x] 
+        
         return self.strategie.prediction(x)
 
     def erreur(self, t, prediction):
@@ -52,10 +59,80 @@ class ClassifieurLineaire:
         x_train, t_train : donnees d'entrainement
         x_test, t_test : donnees de test
         """
-        self.strategie.afficher(x_train, t_train, x_test, t_test)
+        #self.strategie.afficher(x_train, t_train, x_test, t_test)
+        self.afficher(x_train, t_train, x_test, t_test)
 
     def parametres(self):
         """
         Retourne les paramètres du modèle
         """
         return self.strategie.parametres()
+
+    def afficher(self, x_train, t_train, x_test, t_test):
+        le = LabelEncoder()
+        t_train_encoded = le.fit_transform(t_train)
+        t_test_encoded = le.transform(t_test)
+
+        h = 0.05
+        x_min, x_max = x_train[:, 0].min() - .5, x_train[:, 0].max() + .5
+        y_min, y_max = x_train[:, 1].min() - .5, x_train[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+        # Utiliser LinearNDInterpolator pour interpoler les données
+        points = np.column_stack((x_train[:, 0], x_train[:, 1]))
+        values = x_train[:,2:]
+        
+        interpolator = LinearNDInterpolator(points, values)
+        grid_xy = np.c_[xx.ravel(), yy.ravel()]
+        grid_dim = interpolator(grid_xy)
+        grid_tot = np.c_[grid_xy,grid_dim]
+        grid_tot[np.isnan(grid_tot)] = 0
+        grid_z = self.prediction(grid_tot)
+        
+        Z = le.transform(grid_z)
+        # Remettre les résultats en forme pour le tracé
+        Z = Z.reshape(xx.shape)
+
+        
+
+        plt.figure(figsize=(14, 8))
+        plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+        plt.scatter(x_train[:, 0], x_train[:, 1], c=t_train_encoded, edgecolors='k', cmap=plt.cm.Paired)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xticks(())
+        plt.yticks(())
+
+        plt.title('Frontières de décision - Ensemble d\'Entraienement')
+        plt.show()    
+        
+        h = 0.05
+        x_min, x_max = x_test[:, 0].min() - .5, x_test[:, 0].max() + .5
+        y_min, y_max = x_test[:, 1].min() - .5, x_test[:, 1].max() + .5
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+        # Utiliser LinearNDInterpolator pour interpoler les données
+        points = np.column_stack((x_test[:, 0], x_test[:, 1]))
+        values = x_test[:,2:]
+        
+        interpolator = LinearNDInterpolator(points, values)
+        grid_xy = np.c_[xx.ravel(), yy.ravel()]
+        grid_dim = interpolator(grid_xy)
+        grid_tot = np.c_[grid_xy,grid_dim]
+        grid_tot[np.isnan(grid_tot)] = 0
+        grid_z = self.prediction(grid_tot)
+        
+        Z = le.transform(grid_z)
+        # Remettre les résultats en forme pour le tracé
+        Z = Z.reshape(xx.shape)
+
+        plt.figure(figsize=(14, 8))
+        plt.pcolormesh(xx, yy, Z, cmap=plt.cm.Paired)
+        plt.scatter(x_test[:, 0], x_test[:, 1], c=t_test_encoded, edgecolors='k', cmap=plt.cm.Paired)
+        plt.xlim(xx.min(), xx.max())
+        plt.ylim(yy.min(), yy.max())
+        plt.xticks(())
+        plt.yticks(())
+
+        plt.title('Frontières de décision - Données de test')
+        plt.show()    
