982f9b0b4008bb4231db4ffb904895fa3564b640
ValentinLH <98545666+ValentinLH@users.noreply.github.com>
2023-11-28 14:01:15 -0500

    Update main.py

--- Projet_de_Session_IFT712/main.py
+++ Projet_de_Session_IFT712/main.py
@@ -20,9 +20,9 @@ X = scaler.fit_transform(X)
 # Diviser les données en ensembles d'entraînement et de test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-#strategie_perceptron = Perceptron(learning_rate=0.01, max_iterations=1000)
+strategie_perceptron = Perceptron(learning_rate=0.01, max_iterations=1000)
 #strategie_perceptron = RandomForest()
-strategie_perceptron = RandomForestAvecACP()
+#strategie_perceptron = RandomForestAvecACP()
 classifieur = ClassifieurLineaire(strategie_perceptron)
 
 '''strategie_SVM = SVM(kernel='linear', C=1.0)
