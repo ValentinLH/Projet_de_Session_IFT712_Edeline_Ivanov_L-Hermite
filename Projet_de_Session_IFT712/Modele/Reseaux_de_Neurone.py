f9daafc653d61b5f59c1b3bd8d1768f294b3947d
Maxime EDELINE <maxime.edeline@etu.univ-tours.fr>
2023-12-10 15:20:43 -0500

    Commentaire MLP fait

--- Projet_de_Session_IFT712/Modele/Reseaux_de_Neurone.py
+++ Projet_de_Session_IFT712/Modele/Reseaux_de_Neurone.py
@@ -1,16 +1,26 @@
 import sklearn
 from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
 from Modele.ClassifieurLineaire import StrategieClassification
-from Modele.data import TrainData
 import numpy as np
 
 class Reseaux_Neurones(StrategieClassification) :
 
     def __init__(self,hidden_layer_size = (48,12,3),solver = "adam", activation = "relu" ,alpha = 0.0001,learning_rate = "constant",learning_rate_init=0.001,max_iter=500,beta_1=0.9, beta_2=0.999, epsilon=1e-08, momentum = 0.9, power_t = 0.5, max_fun = 15000):
-
+        """
+        :param hidden_layer_size: structure du reseau de neurone
+        :param solver: le type de solveur utilise pour que le modele apprend
+        :param activation: la fontion d'activation
+        :param alpha: le taux de regularisartion utilise par le reseaux de neurone
+        :param learning_rate: le type du pas d'apprentissage
+        :param learning_rate_init: la valeur initiale du pas d'apprentissage
+        :param max_iter: le nombre d'iterations maximum que peut realiser le modele
+        :param beta_1: la valeur du parametre beta1 pour Adam
+        :param beta_2: la valeur du parametre beta2 pour Adam
+        :param epsilon: la valeur du parametre epsilon pour Adam
+        :param momentum: la valeur du momentum pour la descente de gradient stochastique
+        :param power_t : la valeur l'exposent pour le "inverse scaling learning rate"
+        :param max_fun: la valeur maximum d'appel que peut faire le solveur lbfgs
+        """
         #Parametre generaux du MLP
         self.hidden_layer_size = hidden_layer_size
         self.solver = solver
@@ -40,6 +50,12 @@ class Reseaux_Neurones(StrategieClassification) :
         self.W0 = None
 
     def entrainer(self, x_train, t_train):
+        """
+        Entraine le modele de classification Multi layer Perceptron de scikit-learn.
+
+        :param x_train: Les donnees d'entraînement.
+        :param t_train: Les étiquettes de classe cibles.
+        """
 
         self.MLP_model = MLPClassifier(hidden_layer_sizes= self.hidden_layer_size, solver=self.solver , activation=self.activation, alpha=self.alpha,learning_rate = self.learning_rate ,learning_rate_init=self.learning_rate_init, max_iter=self.max_iter, beta_1=self.beta_1, beta_2=self.beta_2, epsilon=self.epsilon,momentum = self.momentum, power_t = self.power_t,max_fun=self.max_fun)
         self.MLP_model = self.MLP_model.fit(x_train,t_train)
@@ -47,13 +63,32 @@ class Reseaux_Neurones(StrategieClassification) :
         self.W = self.MLP_model.coefs_
 
     def prediction(self, X):
+        """
+        Predit la classe de nouvelle donnees X.
+
+        :param X: Les donnees d'entree à classifier.
+        :return: retourne les classes associes aux données
+        """
         predictions = [self.MLP_model.predict([x]) for x in X]
         return predictions
 
     def erreur(self, t, prediction):
+        """
+           Calcule l'erreur de classification.
+
+           :param t: etiquette de classe.
+           :param prediction: La prediction du modele.
+           :return: 1 si l'erreur est commise, 0 sinon.
+       """
+
         return 1 if t != prediction else 0
 
     def parametres(self):
+        """
+            Retourne les parametres du classifieur
+
+            :return: dictionnaire composé des parametres associé à leur valeur
+        """
         return self.W0, self.W
     def afficher(self, x_train, t_train, x_test, t_test):
         #Pas besoin de la coder car il y a un affichage generique
@@ -67,7 +102,7 @@ class Reseaux_Neurones(StrategieClassification) :
         """
         #parametre generaux du MLPClassifier
         learning_rate_type_liste = ["constant", "invscaling", "adaptive"]
-        learning_rate_liste = np.linspace(0.001, 1, 5)  # np.array([0.01]) #np.logspace(-4, 0, 5) #np.linspace(0.001, 0.01, 10)  # np.array([0.01])
+        learning_rate_liste = np.linspace(0.001, 1, 5)
         max_iterations_liste = np.linspace(200, 1000, 5).astype(int)
         fonction_activation = ["tanh", "relu"]
         solveur_liste = ["lbfgs", "adam","sgd"]
